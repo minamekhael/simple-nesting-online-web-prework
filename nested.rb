88a021111aa1906825169096c1c86456435e33fb
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-12 00:25:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,9 +2,7 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
-        :known_for => "COBOL", 
-        :languages => ["COBOL", "FORTRAN"]
+        :grace_hopper => {known_for: "COBOL", languages: ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
