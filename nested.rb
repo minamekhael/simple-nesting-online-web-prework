6ee1a96fb4ddcd39fe7d63f810d40e75f7be5d23
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-12 00:27:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -4,11 +4,9 @@ def hopper
  		{
         :grace_hopper => {known_for: "COBOL", languages: ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {known_for: "Object Orientation", languages => ["Smalltalk", "LISP"]
+        :alan_kay => {known_for: "Object Orientation", languages: ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+        :dennis_ritchie => {known_for: "Unix", languages: ["C"]
         }
      }
 end
